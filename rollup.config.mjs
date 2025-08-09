3f28f27f77deb3954f1de28b3631c3bd11c9ab4e
aldia_mechdream <futami16237@gmail.com>
2025-08-09 14:26:09 +0800

    chore: 微调版本信息、调整exports配置，拥抱现代化

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -21,7 +21,7 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
+        file: 'dist/index.mjs',
         format: 'esm',
         sourcemap: false,
       },
